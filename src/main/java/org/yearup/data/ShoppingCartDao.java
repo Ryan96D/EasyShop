c541031ecdba9763bd35b8ec77730c9640d30496
Ryan <doryan6996@gmail.com>
2025-06-25 18:33:13 -0700

    Update ShoppingCartDao.java
    first draft

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,19 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
+    // GET /cart
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+
+    // POST /cart/products/{id}
+    void addProductToCart(int userId, int productId);
+
+    // PUT /cart/products/{id}
+    void updateQuantity(int userId, int productId, int quantity);
+
+    // DELETE /cart
+    void clearCart(int userId);
 }
