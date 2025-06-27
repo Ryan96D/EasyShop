4fe8dd25bbf70b75178c8c995b99314bf8b9da8b
Ryan <doryan6996@gmail.com>
2025-06-27 02:49:19 -0700

    Create ReceiptDao.java

---
+++ src/main/java/org/yearup/data/ReceiptDao.java
@@ -0,0 +1,11 @@
+package org.yearup.data;
+
+import org.yearup.models.Receipt;
+import org.yearup.models.CheckoutItems;
+
+import java.util.List;
+
+public interface ReceiptDao
+{
+    Receipt createReceipt(int userId, List<CheckoutItems> items);
+}
