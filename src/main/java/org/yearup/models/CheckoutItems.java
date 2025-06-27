0eda51c1f3129c284f0a6f1cb8eb31c76f19e08e
Ryan <doryan6996@gmail.com>
2025-06-27 02:48:25 -0700

    Data models
    Data classes with Getters and Setters

---
+++ src/main/java/org/yearup/models/CheckoutItems.java
@@ -0,0 +1,43 @@
+package org.yearup.models;
+
+import java.math.BigDecimal;
+
+public class CheckoutItems {
+    private int productId;
+    private int quantity;
+    private BigDecimal price;
+    private String productName;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+}
---
+++ src/main/java/org/yearup/models/Receipt.java
@@ -0,0 +1,53 @@
+package org.yearup.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Receipt {
+    private int receiptId;
+    private int userId;
+    private LocalDateTime date;
+    private BigDecimal total;
+    private List<CheckoutItems> items;
+
+    public int getReceiptId() {
+        return receiptId;
+    }
+
+    public void setReceiptId(int receiptId) {
+        this.receiptId = receiptId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public List<CheckoutItems> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CheckoutItems> items) {
+        this.items = items;
+    }
+}
